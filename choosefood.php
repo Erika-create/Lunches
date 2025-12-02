384954d8cb12fe74732964c0f4b6fd972a3983aa
erika-create <okuerika@gmail.com>
2025-12-02 11:15:03 +0000

    basketarray

--- choosefood.php
+++ choosefood.php
@@ -1,29 +1,52 @@
 <?php
     session_start();
-    //print_r($_SESSION);
-    if (!isset($_SESSION["Username"])){
+    #print_r($_SESSION);
+    #NOTE RESTRICT ACCESS IF NOT LOGGED IN LATER
+    if (isset($_SESSION["loggedinuser"])){
         echo("Hello ".$_SESSION["firstname"]);
     }else{
-        echo("Not logged in");
+        echo("not logged in");
     }
 ?>
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <title>Packed lunch ordering system</title>
+<head>          
+    <title>Packed lunch ordering system</title>
 </head>
 
 <body>
     <h1>Orders page</h1>
-    Select category
     <?php
+        session_start();
+        $howmany=count($_SESSION["lunchbasket"]);
+        include_once("connection.php");
+        foreach ($_SESSION["lunchbasket"] as $item){
+            echo($item["foodid"]);
+           
+            $fid=$item["foodid"];
+            $stmt=$conn->prepare("SELECT * FROM tblfood WHERE FoodID=:fid");
+            $stmt->bindParam(":fid",$fid);
+            $stmt->execute();
+            while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+            {
+                print_r($row);
+                echo("<br>");
+            }
+            
+        }
+        
+        
+    ?>
+    Select category 
+    show foods in that category 
+     <?php
         include_once("connection.php");
         $stmt=$conn->prepare("SELECT * FROM tblfood ORDER BY Category, Name");
         $stmt->execute();
         while($row=$stmt->fetch(PDO::FETCH_ASSOC))
         {
             //print_r($row);
-            echo('<form action="addtobasket.php" method="post">');
+            echo ('<form action="addtobasket.php" method="POST">');
             echo($row["Name"]." ".$row["Description"]." ".$row["Price"]);
             echo('<input type="hidden" name="foodid" value='.$row["FoodID"].'>');
             echo('Quantity:<input type="number" name="qty" min="1" max="5" value="1">');
@@ -31,5 +54,6 @@
             echo("<br></form>");
         }
     ?>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <?php
     session_start();
-    //print_r($_SESSION);
-    if (!isset($_SESSION["Username"])){
+    #print_r($_SESSION);
+    if (isset($_SESSION["loggedinuser"])){
         echo("Hello ".$_SESSION["firstname"]);
     }else{
-        echo("Not logged in");
+        echo("not logged in");
     }
 ?>
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <title>Packed lunch ordering system</title>
+<head>          
+    <title>Packed lunch ordering system</title>
 </head>
 
 <body>
-    <h1>Main Page</h1>
+    <h1>Main page</h1>
     <a href="users.php">Add user</a><br>
     <a href="food.php">Add food</a><br>
-    <a href="choosefood.php">Choose food</a><br>
+    <a href="choosefood.php">choose food</a><br>
     <a href="login.php">Login</a><br>
     <a href="logout.php">Logout</a><br>
+    <a href="emptybasket.php">Empty Basket</a><br>
 </body>
 </html>
