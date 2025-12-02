2b37f189d489797da5aa7cc6589adb5ef55f0dae
erika-create <okuerika@gmail.com>
2025-12-02 11:24:08 +0000

    viewbasketpage

--- choosefood.php
+++ choosefood.php
@@ -19,6 +19,7 @@
     <?php
         session_start();
         $howmany=count($_SESSION["lunchbasket"]);
+        echo("You have ".$howmany." items in your basket<br>");
         include_once("connection.php");
         foreach ($_SESSION["lunchbasket"] as $item){
             echo($item["foodid"]);
